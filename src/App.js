cb230e854dea52e2d49d0d2ff0f36c352529272b
ueii <kjh2623kjh@gmail.com>
2024-09-30 23:39:11 +0900

    feat: Create MainScreen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { styled } from "styled-components";
+
 import GlobalFonts from "./styles/GlobalFonts.js";
 import GlobalStyles from "./styles/GlobalStyles.js";
 import Home from "./pages/Home.js";
@@ -15,12 +17,23 @@ const router = createBrowserRouter(
 
 function App() {
   return (
-    <>
+    <MainScreen>
       <RouterProvider router={router} />
       <GlobalFonts />
       <GlobalStyles />
-    </>
+    </MainScreen>
   );
 }
+
+const MainScreen = styled.div`
+  width: 360px;
+  height: 800px;
+  margin-right: auto;
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  align-items: center;
+`;
 
 export default App;
