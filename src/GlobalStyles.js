857552d67533a0dd33d7de104630e38545f37d30
ueii <kjh2623kjh@gmail.com>
2024-11-01 17:08:15 +0900

    feat: Create GlobalColor Object

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,5 +1,48 @@
 import { createGlobalStyle } from "styled-components";
 
+const GlobalColor = {
+  Main: {
+    100: "#276C2D",
+    90: "#34823A",
+    80: "#489E4F",
+    70: "#5AC563", // main
+    main: "#5AC563",
+    60: "#7BD182",
+    50: "#9CDCA1",
+    40: "#BDE8C1",
+    30: "#CDEED0",
+    20: "#DEF3E0",
+    10: "#EEF9EF",
+  },
+  GrayScale: {
+    130: "#011327", // main
+    main: "#011327",
+    120: "#292e41",
+    110: "#373f57",
+    100: "#475067",
+    90: "#5c667b",
+    80: "#67738e",
+    70: "#8491a7",
+    60: "#96a0b5",
+    50: "#b4c0d3",
+    40: "#d7dce5",
+    30: "#eaedf4", // border
+    border: "#eaedf4",
+    20: "#f4f6fa",
+    10: "#f8fafc",
+  },
+  System: {
+    ERROR: "#F04452",
+    SUCCESS: "#5AC563",
+    INFO: "#4A7FF7",
+  },
+  black: "#000000",
+  white: "#ffffff",
+  red: "#F04452",
+  green: "#F04452",
+  blue: "#4A7FF7",
+};
+
 const GlobalStyles = createGlobalStyle`
   body {
     background-color: rgb(255, 252, 230);
@@ -74,4 +117,4 @@ const GlobalFonts = createGlobalStyle`
 }
 `;
 
-export { GlobalStyles, GlobalFonts };
+export { GlobalStyles, GlobalFonts, GlobalColor };
