846f7d673869dda1aa7b0cb0f032a5e2a198868d
ueii <kjh2623kjh@gmail.com>
2024-11-05 21:46:24 +0900

    feat: Update Advertise Box

--- src/components/home/AdvertiseBox.js
+++ src/components/home/AdvertiseBox.js
@@ -2,17 +2,42 @@ import styled from "styled-components";
 
 import advertise from "../../assets/advertisebox/advertise-example.png";
 
+import { GlobalColor } from "../../GlobalStyles";
+
+const advertise_example = [advertise, advertise, advertise];
+
 export default function AdvertiseBox() {
   return (
     <Container>
-      <div></div>
-      <div></div>
+      <Paging />
+      <Slider>
+        {/* {advertise_example.map((img, i) => (
+          <img src={img} />
+        ))} */}
+      </Slider>
     </Container>
   );
 }
 
 const Container = styled.div`
   border-radius: 10px;
-  height: 122px;
-  background-color: white;
+  height: 150px;
+  background-color: ${GlobalColor.GrayScale[40]};
+`;
+
+const Slider = styled.div`
+  // 나중에 슬라이드 되게 바꿔야 함
+
+  // display: flex;
+  // flex-wrap: nowrap;
+
+  img {
+    width: 100%;
+    height: 100%;
+    overflow: hidden; // 보더까지 hidden되게 하고싶다.
+  }
+`;
+
+const Paging = styled.div`
+  // 나중에 추가
 `;
