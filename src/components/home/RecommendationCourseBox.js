d35acb980e8ead558e85628d5a30b722c29809df
ueii <kjh2623kjh@gmail.com>
2024-11-05 21:47:36 +0900

    feat: Update RecommendationCourseBox

--- src/components/home/RecommendationCourseBox.js
+++ src/components/home/RecommendationCourseBox.js
@@ -1,82 +1,130 @@
-// 임시 코드
+import { useState } from "react";
 
 import styled from "styled-components";
 
 import mountainImage from "../../assets/home/MountainImage.jpg";
-import { ReactComponent as RightArrow } from "../../assets/home/RightArrow.svg";
+import { ReactComponent as Favorite } from "../../assets/home/Favorite.svg";
+import { GlobalColor } from "../../GlobalStyles";
 
 const mountainData = [
   {
+    id: 1,
     name: "응봉산",
-    courses: ["팔각정 코스"],
+    course: "팔각정 코스",
     image: mountainImage,
   },
   {
-    name: "아차산",
-    courses: ["정상길 코스", "용마산-망우산 연계산행코스"],
+    id: 2,
+    name: "청계산",
+    course: "원터골 입구 코스",
     image: mountainImage,
   },
   {
+    id: 3,
     name: "불암산",
-    courses: ["정상길 코스", "용마산-망우산 연계산행코스"],
+    course: "원터골 입구 코스",
     image: mountainImage,
   },
   {
-    name: "북한산",
-    courses: ["정상길 코스", "용마산-망우산 연계산행코스"],
+    id: 4,
+    name: "속리산",
+    course: "문장대 코스",
+    image: mountainImage,
+  },
+  {
+    id: 5,
+    name: "속리산",
+    course: "문장대 코스",
     image: mountainImage,
   },
 ];
 
 export default function RecommendationCourseBox() {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  const onClick = (id) => {
+    if (userFavorites.includes(id)) {
+      setUserFavorites(userFavorites.filter((element) => element != id));
+    } else setUserFavorites([...userFavorites, id]);
+  };
+
   return mountainData.map((element) => (
-    <Box>
-      <img src={element.image} alt="mountain" />
+    <Box style={{ backgroundImage: `url(${element.image})` }}>
+      <div className="gradation" />
       <Title>{element.name}</Title>
-      {element.courses.map((course) => (
-        <Course>
-          {course} <RightArrow />
-        </Course>
-      ))}
+      <Course>{element.course}</Course>
+      <FavoriteButton onClick={() => onClick(element.id)}>
+        {userFavorites.includes(element.id) ? (
+          <Favorite fill="red" stroke="red" />
+        ) : (
+          <Favorite fill="none" stroke="white" />
+        )}
+      </FavoriteButton>
     </Box>
   ));
 }
 
 const Box = styled.div`
-  padding: 10px;
+  aspect-ratio: 1; // 가로세로 비율
+  width: calc(100% / 2 - 5px);
+  z-index: 0;
+  background-repeat: no-repeat;
+
+  border-radius: 8px;
+  background: #d9d9d9;
   margin-bottom: 10px;
 
-  border-radius: 10.231px;
-  border: 0.853px solid var(--background, #efeff0);
-  background: var(--white, #fff);
-  box-shadow: 0px 2.558px 3.41px 0px rgba(0, 0, 0, 0.25);
+  position: relative;
 
-  img {
-    width: 120px;
-    height: 80px;
+  &:after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: calc(100% * 0.8);
     flex-shrink: 0;
+    z-index: -1;
+    position: relative;
+    // top: -24px;
 
-    border-radius: 6.821px;
-    border: 1.705px solid var(--subtle-light, #afb1b6);
-    background: url(<path-to-image>) lightgray 50% / cover no-repeat;
+    border-radius: 8px 8px 0px 0px;
+    background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0.8) 100%
+    );
   }
 `;
 
 const Title = styled.div`
-  color: var(--color-foundation-grey-normal-active, #242424);
-  font-family: Paperlogy-7;
-  font-size: 13.642px;
-  font-style: normal;
-  line-height: 150%; /* 20.462px */
+  position: absolute;
+  top: 12px;
+  left: 16px;
+
+  color: ${GlobalColor.white};
+  font-feature-settings: "liga" off, "clig" off;
+  font-family: Pretendard-7;
+  font-size: 16px;
+  line-height: 150%; /* 24px */
+  letter-spacing: -0.2px;
 `;
 
 const Course = styled.div`
-  color: #686868;
-  font-family: Paperlogy-7;
-  font-size: 10.231px;
-  font-style: normal;
-  line-height: 150%; /* 15.347px */
+  position: absolute;
+  top: 38px;
+  left: 16px;
 
-  display: flex;
-  align-items: center;
+  color: ${GlobalColor.white};
+  font-feature-settings: "liga" off, "clig" off;
+  font-family: Pretendard-5;
+  font-size: 13px;
+  line-height: 150%; /* 19.5px */
+  letter-spacing: -0.162px;
+`;
+
+const FavoriteButton = styled.div`
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
 `;
